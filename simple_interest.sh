63a0743c5b4fbcb2ae30be75b16ef80079771bb9
asdeth54635y47u7u <143603537+asdeth54635y47u7u@users.noreply.github.com>
2023-08-30 12:21:19 +0200

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Wpisz wartość kapitału pożyczki: "
+read kapital
+echo "Wpisz RRSO(bez znaku procent): "
+read rrso
+echo "Wpisz ilosc miesięcy: "
+read okres
+
+rrso=$(expr rrso/100)
+odestki=$(expr $kapital \* $rrso \* ($okres/12) )
+echx "Odsetki: " $odestki
+ilosc=$(expr $simple_interest + $kapital)
+echo "Całkowita spłata pożyczki to: "$amount
